95b0d1ea8f380959dfecb10f0996abdb20b09e2d
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-02-14 13:34:09 +0000

    Add a getComputerChoice function
    Returns "rock", "paper" or "scissors" randomly

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+function getComputerChoice() {
+  // Randomly returns "rock", "paper" or "scissors"
+
+  // Get random number 1 - 3
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  // Return a choice depending on the number
+  if (randomNumber === 1) {
+    return 'rock';
+  } else if (randomNumber === 2) {
+    return 'paper';
+  } else if (randomNumber === 3) {
+    return 'scissors';
+  }
+}
+
+console.log(getComputerChoice());
